7c31cad6376ad67b404212767e9ed26949fa3e62
DESKTOP-UOTJFFC\Isaid <iarena@ia.com.mx>
2023-04-28 17:14:42 -0600

    Feat: Manage Errors
    Add Middleware for Manage Error and excepcitions

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApi.DTOs;
+using WebApi.Middleware;
 
 namespace WebApi
 {
@@ -22,6 +24,8 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(MappingProfiles));
+
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             services.AddDbContext<MarketDbContext>(cfg =>
@@ -35,10 +39,13 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors", "?statusCode={0}");
 
             app.UseRouting();
 
